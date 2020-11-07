fb3036fc3d116acc968322827696f8d180efdd0e
ariane <ariianeboorges@gmail.com>
2020-11-07 13:41:56 +0000

    Remoção de transação

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -3,7 +3,7 @@ import { getCustomRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -13,6 +13,7 @@ transactionsRouter.get('/', async (request, response) => {
 
   const transactions = await transactionsRepository.find();
   const balance = await transactionsRepository.getBalance();
+
   return response.json({ transactions, balance });
 });
 
@@ -31,9 +32,15 @@ transactionsRouter.post('/', async (request, response) => {
   return response.json(transaction);
 });
 
-// transactionsRouter.delete('/:id', async (request, response) => {
-//   // TODO
-// });
+transactionsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute(id);
+
+  return response.status(204).send();
+});
 
 // transactionsRouter.post('/import', async (request, response) => {
 //   // TODO
